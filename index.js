cc457fcebf155804eee89928d9e85301ed5f919c
Fabio Moreno <fabiomoreno@outlook.com>
2024-02-20 13:57:30 -0500

    feat: add logs

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send({
-      message: 'CCP demo app is working!',
-      version: '20210821.1',
-      branch: 'main',
-      env: process.env.env_demo || 'this variables was not loaded'
-  })
+  let response = {
+    message: 'CCP demo app is working!',
+    version: '20210821.1',
+    branch: 'main',
+    env: process.env.env_demo || 'this variables was not loaded',
+    time: new Date().toString()
+  }
+
+  console.log(JSON.stringify(response))
+  res.send(response)
 })
 
 app.listen(port, () => {
   console.log(`CCP demo app listening at http://0.0.0.0:${port}`)
+  // log
+  setInterval(function(){
+    console.log("app continue running ", new Date().toString())
+  }, 1000)
 })
